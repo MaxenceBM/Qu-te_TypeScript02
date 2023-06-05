ff474eaccdf768f92064dd24dffc3ca15299c217
Maxence BLUM <maxenceblum67@gmail.com>
2023-06-05 18:28:11 +0200

    Quete02_TS

---
+++ javac/Photo.java
@@ -0,0 +1,6 @@
+public class Photo {
+    private String url;
+    private String filename;
+    private Number width;
+    private Number height;
+}
---
+++ javac/User.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class User {
+    private Date birthday;
+    private Number size;
+    private Photo[] photos;
+    private Address address;
+    
+    public static void main(String[] args) {
+        // Code à exécuter ici
+        System.out.println("Programme exécuté avec succès !");
+    }
+}
